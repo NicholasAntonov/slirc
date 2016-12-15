b82e68537c1371fa7fa4f7be0d92bdc52bba4c74
jajmo <jajmo@stevens.edu>
2016-12-14 21:41:28 -0500

    Add readme

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
 var exports = module.exports = {};
 
 exports.rethink = {
-    host: "127.0.0.1",
-    port: 28015,
-    db: "slirc"
+    host: "127.0.0.1", // Rethink IP
+    port: 28015, // Rethink port
+    db: "slirc" // Rethink database
 };
 
 exports.serverConfig = {
-    port: 3000, 
-    sessionSecret: "somesupersecretkey" // Used for generating auth tokens
+    port: 3000, // What port should express listen on?
+    sessionSecret: "somesupersecretkey" // Used for generating auth tokens. Should be something secure
 }
